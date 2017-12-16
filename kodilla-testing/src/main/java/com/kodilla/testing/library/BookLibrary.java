8fb525758611d09a57baa94d2216d45ba708d425
MarcinMG <mmgrabowski@o2.pl>
2017-12-16 19:03:05 +0100

    poprawka 6.6

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -20,19 +20,7 @@ public class BookLibrary {
     }
 
     public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
-        List<Book> bookList = new ArrayList<Book>();;
         List<Book> resultList1 = libraryDatabase.listBooksInHandsOf(libraryUser);
-
-        if(libraryUser.peselId == "1234567890") {
-            return resultList1;
-        }
-        else if(libraryUser.peselId == "1111111111") {
-            return resultList1;
-        }
-        else if(libraryUser.peselId == "5555555555") {
-            return resultList1;
-        }
-        else return bookList;
-
+        return resultList1;
     }
 }
