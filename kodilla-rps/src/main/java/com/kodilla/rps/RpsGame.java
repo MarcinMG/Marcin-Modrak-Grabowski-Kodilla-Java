fec89869433a6d30e78e5fcc5a05f8ce95fd3bbd
MarcinMG <mmgrabowski@o2.pl>
2018-01-13 16:45:41 +0100

    Zad7.7 popr

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsGame.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsGame.java
@@ -6,14 +6,12 @@ public class RpsGame {
 
     private RpsRules rpsRules;
     private boolean end = false;
+    private boolean newGame = false;
     private int actualRound = 1;
-    private int computerChoice;
     private int playerScore = 0;
     private int computerScore = 0;
+    private String winner = "brak";
     private String playerChoice;
-    private String quitGame = "n";
-    private String newGame = "n";
-    private String computerChoiceString;
 
     public RpsGame(RpsRules rpsRules) {
         this.rpsRules = rpsRules;
@@ -33,7 +31,8 @@ public class RpsGame {
         actualRound = 1;
         playerScore = 0;
         computerScore = 0;
-        newGame = "n";
+        newGame = false;
+        winner = "brak";
     }
 
     public void setPlayerScore() {
@@ -50,43 +49,61 @@ public class RpsGame {
     public String toString() {
         return "Podsumowanie gry: " + '\n' +
                 "Rundy: " + actualRound + '\n' +
-                "Wynik: " + playerScore + " : " + computerScore;
+                "Wynik: " + playerScore + " : " + computerScore + '\n' +
+                "Wygrał: " + winner;
     }
 
-    public String startNewGame() {
-
-        String result = "quit";
-        RpsRandom random = new RpsRandom();
+    public boolean endGame() {
+        String quitGame;
+        System.out.println("Czy na pewno zakończyć grę? T-tak N-nie");
         Scanner scanner = new Scanner(System.in);
+        do {
+            quitGame = scanner.next();
+        } while ((!quitGame.equals("t")) && (!(quitGame.equals("n"))));
+        return (quitGame.equals("t"));
+    }
 
+    public boolean exitMenu() {
+        String exitGame;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            exitGame = scanner.next();
+        } while ((!exitGame.equals("x")) && (!(exitGame.equals("n"))));
+        return (exitGame.equals("x"));
+    }
+
+    public void yourMove() {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print(rpsRules.getPlayerName() + ", Twój ruch: ");
+            playerChoice = scanner.next();
+            if (playerChoice.equals("1")) {
+                playerChoice = "KAMIEŃ";
+            } else if (playerChoice.equals("2")) {
+                playerChoice = "PAPIER";
+            } else if (playerChoice.equals("3")) {
+                playerChoice = "NOŻYCE";
+            } else if (playerChoice.equals("x")) {
+                end = endGame();
+            } else if (playerChoice.equals("n")) {
+                newGame = endGame();
+            }
+
+        } while (!(playerChoice.equals("KAMIEŃ")) && !(playerChoice.equals("PAPIER"))
+                && !(playerChoice.equals("NOŻYCE")) && !end && !newGame);
+    }
+
+    public void startNewGame() {
+
+        int computerChoice;
+        String computerChoiceString;
+        RpsRandom random = new RpsRandom();
         showRules();
 
         while(!end) {
 
             System.out.println("Runda nr: " + actualRound);
-            do {
-                System.out.print(rpsRules.getPlayerName() + ", Twój ruch: ");
-                playerChoice = scanner.next();
-                if (playerChoice.equals("1")) {
-                    playerChoice = "KAMIEŃ";
-                } else if (playerChoice.equals("2")) {
-                    playerChoice = "PAPIER";
-                } else if (playerChoice.equals("3")) {
-                    playerChoice = "NOŻYCE";
-                } else if (playerChoice.equals("x")) {
-                    System.out.println("Czy na pewno zakończyć grę? T-tak N-nie");
-                    do {
-                        quitGame = scanner.next();
-                    } while ((!quitGame.equals("t")) && (!(quitGame.equals("n"))));
-                } else if (playerChoice.equals("n")) {
-                    System.out.println("Czy na pewno zakończyć aktualną grę?? T-tak N-nie");
-                    do {
-                        newGame = scanner.next();
-                    } while ((!newGame.equals("t")) && (!(newGame.equals("n"))));
-                }
-
-            } while (!(playerChoice.equals("KAMIEŃ")) && !(playerChoice.equals("PAPIER"))
-                    && !(playerChoice.equals("NOŻYCE")) && !(quitGame.equals("t")) && !(newGame.equals("t")));
+            yourMove();
 
             computerChoice = random.GetRandom();
             if (computerChoice == 1) {
@@ -95,7 +112,7 @@ public class RpsGame {
                 computerChoiceString = "PAPIER";
             } else computerChoiceString = "NOŻYCE";
 
-            if (!(quitGame.equals("t")) && !(newGame.equals("t"))) {
+            if (!end && !newGame) {
                 System.out.print("Gracz: " + playerChoice + " Komputer: " + computerChoiceString);
 
                 if (!playerChoice.equals(computerChoiceString)) {
@@ -121,21 +138,15 @@ public class RpsGame {
             if ((playerScore == rpsRules.getNumberOfWins()) || (computerScore == rpsRules.getNumberOfWins())) {
                 end = true;
                 if (playerScore > computerScore) {
-                    result = "Gracz";
-                } else {
-                    result = "komputer";
+                    winner = "Gracz";
+                }else {
+                    winner = "Komputer";
                 }
-            }else actualRound++;;
+            }else actualRound++;
 
-            if (quitGame.equals("t")) {
-                System.exit(0);
-            }
-
-            if (newGame.equals("t")) {
+            if (newGame) {
                 getReady();
             }
         }
-
-        return result;
     }
 }
--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -1,27 +1,19 @@
 package com.kodilla.rps;
 
-import java.util.Scanner;
-
 public class RpsRunner {
     public static void main(String[] args) {
 
-        String result;
-        Scanner scanner = new Scanner(System.in);
-
+        Boolean result;
         RpsRules rules = new RpsRules();
 
         do {
             rules.getInfo();
             RpsGame game = new RpsGame(rules);
-            result = game.startNewGame();
+            game.startNewGame();
             System.out.println(game);
-            System.out.println("Wygrał: " + result);
             System.out.println("x-koniec gry  n-nowa gra");
-            do {
-                result = scanner.next();
-            } while ((!result.equals("x")) && (!(result.equals("n"))));
-
-        }while (!result.equals("x"));
+            result = game.exitMenu();
+        }while (!result);
 
     }
 }
